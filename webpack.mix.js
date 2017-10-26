c58b633838c1548bb6157d40d60cee305700a336
James Devon <james.devon@digital.justice.gov.uk>
2017-10-26 13:00:18 +0100

    commented out browserSync

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,3 +3,8 @@ const mix = require('laravel-mix');
 mix.js('src/js/main.js', 'public/js')
     .sourceMaps()
     .sass('src/sass/main.sass', 'public/css')
+
+// mix.browserSync({
+//     proxy: 'localhost',
+//     port: 8890
+// });
