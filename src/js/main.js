55b080a7d1505498a81667596b015bcb4abdeec1
James Devon <james.devon@digital.justice.gov.uk>
2017-10-28 10:42:36 +0100

    vueified linked nodes

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Contact from '../vue/Contact.vue'
+import LinkedNodes from '../vue/LinkedNodes.vue'
 import Search from '../vue/Search.vue'
 import TextBlock from '../vue/TextBlock.vue'
 
@@ -48,7 +49,7 @@ window.onload = function() {
             button.addEventListener('click', toggleOpenAccordion.bind(null, elements[i]))
         }
     })();
-
+    Vue.component('linked-nodes', LinkedNodes);
     Vue.component('text-block', TextBlock);
     Vue.component('search', Search);
     Vue.component('contact', Contact);
---
+++ src/vue/LinkedNodes.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="linked-nodes">
+        <!--<h2 class="linked-nodes__title">Application steps</h2>-->
+        <LNTitle>Application steps</LNTitle>
+        <h3 class="linked-nodes__subtitle">Find out everything you need to know about joining the prison and probation service</h3>
+        <ol class="linked-nodes__list">
+            <li class="linked-nodes__list-element">
+                <div class="linked-nodes__list-element-main-text">Complete online application form</div>
+                <div class="linked-nodes__list-element-subtext">10 minutes</div>
+            </li>
+            <li class="linked-nodes__list-element">
+                <div class="linked-nodes__list-element-main-text">Judgement and numeracy tests</div>
+                <div class="linked-nodes__list-element-subtext">24 minutes</div>
+            </li>
+            <li class="linked-nodes__list-element">
+                <div class="linked-nodes__list-element-main-text">Notification of pass or failure</div>
+                <div class="linked-nodes__list-element-subtext">1 day</div>
+            </li>
+            <li class="linked-nodes__list-element">
+                <div class="linked-nodes__list-element-main-text">Book prison visit</div>
+                <div class="linked-nodes__list-element-subtext">Up to 3 weeks</div>
+            </li>
+            <li class="linked-nodes__list-element">
+                <div class="linked-nodes__list-element-main-text">Recruitment Assessment Day</div>
+                <div class="linked-nodes__list-element-subtext">1 day</div>
+            </li>
+            <li class="linked-nodes__list-element">
+                <div class="linked-nodes__list-element-main-text">Vetting process</div>
+                <div class="linked-nodes__list-element-subtext">Up to 12 weeks</div>
+            </li>
+        </ol>
+    </div>
+</template>
+<script>
+    import Vue from 'vue';
+//    const LNTitle = Vue.component('LNTitle',{
+//      template: '<h2 class="linked-nodes__title">Application steps</h2>'
+//    });
+    const LNTitle = {
+        template: '<h2 class="linked-nodes__title">Application steps</h2>'
+    };
+    export default {
+        props: ['title'],
+        components: {
+            LNTitle
+        }
+    }
+</script>
