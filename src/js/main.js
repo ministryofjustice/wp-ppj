f6c8a6b9eb86bccf624fff32e02c4695c74bfbc9
James Devon <james.devon@digital.justice.gov.uk>
2017-10-30 15:17:08 +0000

    vueified header

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,16 @@
+import Accordion from '../vue/Accordion.vue';
+import Contact from '../vue/Contact.vue';
+import Header from '../vue/Header.vue';
+import OrderedList from '../vue/OrderedList.vue';
+import PageContainer from '../vue/PageContainer.vue';
+import Search from '../vue/Search.vue';
+import TextBlock from '../vue/TextBlock.vue';
 import Vue from 'vue';
-import Contact from '../vue/Contact.vue'
-import Accordion from '../vue/Accordion.vue'
-import OrderedList from '../vue/OrderedList.vue'
-import Search from '../vue/Search.vue'
-import TextBlock from '../vue/TextBlock.vue'
-
-function toggleOpenAccordion(el) {
-    var openClassName = 'accordion__list-element--open';
-    var listElement = el.closest('.accordion__list-element.js');
-    if (listElement.classList.contains(openClassName)) {
-        listElement.classList.remove(openClassName);
-    } else {
-        listElement.classList.add(openClassName);
-    }
-}
 
 function toggleOpenNavMenu(navLink) {
     console.log('toggleOpenNavMenu');
-    var openClassName = 'page-container--nav-menu-open';
-    var pageContainer = navLink.closest('.page-container.js');
+    const openClassName = 'page-container--nav-menu-open';
+    const pageContainer = navLink.closest('.page-container.js');
     if (pageContainer.classList.contains(openClassName)) {
         pageContainer.classList.remove(openClassName);
     } else {
@@ -28,29 +20,25 @@ function toggleOpenNavMenu(navLink) {
 
 window.onload = function() {
 
-    (function addNavClickEventHandler() {
-        var navLink = document.getElementsByClassName('header__nav-link js')[0];
-        navLink.addEventListener('click', toggleOpenNavMenu.bind(null, navLink));
-        var closeNavButton = document.getElementsByClassName('header__nav-menu-close-button js')[0];
-        closeNavButton.addEventListener('click', toggleOpenNavMenu.bind(null, closeNavButton));
-    })();
-
     (function addAccordionFillerContent() {  // TODO remove this before launch
-        var elements = document.getElementsByClassName('accordion__list-element js');
-        for (var i = 0; i < elements.length; i++) {
-            var content = elements[i].getElementsByClassName('accordion__list-element-content')[0];
+        const elements = document.getElementsByClassName('accordion__list-element js');
+        for (let i = 0; i < elements.length; i++) {
+            const content = elements[i].getElementsByClassName('accordion__list-element-content')[0];
             content.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
         }
     })();
 
     Vue.component('accordion', Accordion);
     Vue.component('accordion-element', Accordion.childComponents.Element);
+    Vue.component('contact', Contact);
+    Vue.component('page-header', Header);
     Vue.component('ordered-list', OrderedList);
     Vue.component('ol-element', OrderedList.childComponents.Element);
-    Vue.component('text-block', TextBlock);
+    Vue.component('page-container', PageContainer);
     Vue.component('search', Search);
-    Vue.component('contact', Contact);
+    Vue.component('text-block', TextBlock);
+
     new Vue({
-        el: '.page-container'
+        el: '#site-container'
     })
 };
---
+++ src/vue/Header.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="header"
+         :class="{'header--nav-menu-open': menuOpen}">
+        <div class="header__logo"></div>
+        <div class="header__text">Want a new challenge? We've got your back</div>
+        <nav-link v-on:request-open-nav-menu="openNavMenu"></nav-link>
+        <nav-menu v-on:request-close-nav-menu="closeNavMenu"></nav-menu>
+    </div>
+</template>
+<script>
+    import Vue from 'vue';
+
+    const NavLink = {
+        template:
+            `<div class="header__nav-link js" @click="openNavMenu">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>`,
+        methods: {
+            openNavMenu: function() {
+                console.log('sending request-open-nav-menu');
+                this.$emit('request-open-nav-menu', true);
+            }
+        }
+    };
+
+    const NavMenu = {
+        template:
+            ` <div class="header__nav-menu">
+                <button class="header__nav-menu-close-button js" @click="closeNavMenu"></button>
+                <ul class="header__nav-menu-list">
+                    <li class="header__nav-menu-list-element">Home</li>
+                    <li class="header__nav-menu-list-element">Apply</li>
+                    <li class="header__nav-menu-list-element">Working as a Prison Officer</li>
+                    <li class="header__nav-menu-list-element">Find a role</li>
+                    <li class="header__nav-menu-list-element">Is it right for me</li>
+                    <li class="header__nav-menu-list-element">Working in a prison setting</li>
+                    <li class="header__nav-menu-list-element">Application steps</li>
+                    <li class="header__nav-menu-list-element">Eligibility Checker</li>
+                </ul>
+            </div>`,
+        methods: {
+            closeNavMenu: function() {
+                this.$emit('request-close-nav-menu', true);
+            }
+        }
+    };
+
+    export default {
+        props: ['title', 'subtitle'],
+
+        data() {
+          return {
+              menuOpen: false
+          }
+        },
+
+        components: {
+            'nav-menu': NavMenu,
+            'nav-link': NavLink
+        },
+
+        methods: {
+            openNavMenu() {
+                this.menuOpen = true;
+            },
+            closeNavMenu(){
+                this.menuOpen = false;
+            }
+        },
+
+        childComponents: {
+
+        }
+    }
+</script>
