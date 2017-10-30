a6f2900ea80f552ee8506f551bd11ab0826139b3
James Devon <james.devon@digital.justice.gov.uk>
2017-10-30 17:52:11 +0000

    added google map and jobs list to search section

---
+++ src/vue/PageContainer.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="page-container js"
+         :class="{'page-container--nav-menu-open' : menuOpen}">
+        <slot></slot>
+    </div>
+</template>
+<script>
+    import Vue from 'vue';
+
+    export default {
+        data() {
+            return {
+                menuOpen: false
+            }
+        }
+    }
+</script>
--- src/vue/Search.vue
+++ src/vue/Search.vue
@@ -3,15 +3,108 @@
             <h2 class="search__title">{{ titleText }}</h2>
             <form class="search__form">
                 <input
-                        type="text"
-                        class="search__input"
-                        :placeholder="placeHolderText"
-                        editable="editable"
+                    type="text"
+                    class="search__input"
+                    :placeholder="placeHolderText"
+                    editable="editable"
+                    v-model="searchResults.postCode"
                 />
-                <button class="search__button">
-                    <div class="search__button-circle"></div>
-                    <div class="search__button-rectangle"></div>
+                <button class="search__button-search"
+                        @click.stop.prevent="search">
+                    <div class="search__button-search-circle"></div>
+                    <div class="search__button-search-rectangle"></div>
                 </button>
+                <div class="search__results" v-show="searchResults.display">
+                    <div class="search__view-controls">
+                        <button
+                                class="search__button-view"
+                                :class="{'search__button-view--active': (searchResults.activeView == 0)}"
+                                @click.stop.prevent="showMapView"
+                        >Map view</button>
+                        <button
+                                class="search__button-view"
+                                :class="{'search__button-view--active': (searchResults.activeView == 1)}"
+                                @click.stop.prevent="showListView"
+                        >List view</button>
+                    </div>
+                    <div class="search__view-container">
+                        <div class="search__view-map" v-show="searchResults.activeView == 0">
+                            <div class="search__map">
+                                <iframe width="280"
+                                        height="180"
+                                        frameborder="0"
+                                        style="border:0"
+                                        :src="mapSrc"
+                                        allowfullscreen></iframe>
+                            </div>
+                            <div class="search__job-post">
+                                <div class="search__job-post-distance">0.88 miles</div>
+                                <div class="search__job-post-salary">£31,560</div>
+                                <div class="search__job-post-prison">
+                                    <div class="search__job-post-prison-name">HMP Belmarsh</div>
+                                    <div class="search__job-post-prison-city">London</div>
+                                </div>
+                                <a class="search__job-post-link">view job</a>
+                            </div>
+                        </div>
+                        <ul class="search__view-list" v-show="searchResults.activeView == 1">
+                            <div class="search__job-post">
+                                <div class="search__job-post-distance">0.88 miles</div>
+                                <div class="search__job-post-salary">£31,560</div>
+                                <div class="search__job-post-prison">
+                                    <div class="search__job-post-prison-name">HMP Belmarsh</div>
+                                    <div class="search__job-post-prison-city">London</div>
+                                </div>
+                                <a class="search__job-post-link">view job</a>
+                            </div>
+                            <div class="search__job-post">
+                                <div class="search__job-post-distance">0.88 miles</div>
+                                <div class="search__job-post-salary">£31,560</div>
+                                <div class="search__job-post-prison">
+                                    <div class="search__job-post-prison-name">HMP Belmarsh</div>
+                                    <div class="search__job-post-prison-city">London</div>
+                                </div>
+                                <a class="search__job-post-link">view job</a>
+                            </div>
+                            <div class="search__job-post">
+                                <div class="search__job-post-distance">0.88 miles</div>
+                                <div class="search__job-post-salary">£31,560</div>
+                                <div class="search__job-post-prison">
+                                    <div class="search__job-post-prison-name">HMP Belmarsh</div>
+                                    <div class="search__job-post-prison-city">London</div>
+                                </div>
+                                <a class="search__job-post-link">view job</a>
+                            </div>
+                            <div class="search__job-post">
+                                <div class="search__job-post-distance">0.88 miles</div>
+                                <div class="search__job-post-salary">£31,560</div>
+                                <div class="search__job-post-prison">
+                                    <div class="search__job-post-prison-name">HMP Belmarsh</div>
+                                    <div class="search__job-post-prison-city">London</div>
+                                </div>
+                                <a class="search__job-post-link">view job</a>
+                            </div>
+                            <div class="search__job-post">
+                                <div class="search__job-post-distance">0.88 miles</div>
+                                <div class="search__job-post-salary">£31,560</div>
+                                <div class="search__job-post-prison">
+                                    <div class="search__job-post-prison-name">HMP Belmarsh</div>
+                                    <div class="search__job-post-prison-city">London</div>
+                                </div>
+                                <a class="search__job-post-link">view job</a>
+                            </div>
+                            <div class="search__job-post">
+                                <div class="search__job-post-distance">0.88 miles</div>
+                                <div class="search__job-post-salary">£31,560</div>
+                                <div class="search__job-post-prison">
+                                    <div class="search__job-post-prison-name">HMP Belmarsh</div>
+                                    <div class="search__job-post-prison-city">London</div>
+                                </div>
+                                <a class="search__job-post-link">view job</a>
+                            </div>
+                        </ul>
+                    </div>
+                </div>
             </form>
         </div>
 </template>
@@ -20,9 +113,50 @@
     export default {
         data() {
             return {
+                searchResults: {
+                    activeView: 0,
+                    display: true,
+                    postCode: '',
+                    urlEncodedPostCode: '',
+                    googleMapAPIKey: 'AIzaSyD6bT-hldMJMz4jwUgJ2W1YA-bXpROvKHk',
+
+                },
+                mapSrc: '',
                 titleText: 'Search for vacancies near you',
                 placeHolderText: 'Enter your post code'
             }
+        },
+        watch: {
+            'searchResults.postCode': function(val) {
+                this.searchResults.urlEncodedPostCode = encodeURIComponent(val);
+                console.log('updated urlEncodedPostCode ', this.searchResults.urlEncodedPostCode);
+            },
+            'searchResults.urlEncodedPostCode': function(val) {
+                if (val) {
+                    const result = "https://www.google.com/maps/embed/v1/place?q="
+                        + val
+                        + "&key=" + this.searchResults.googleMapAPIKey;
+                    this.mapSrc = result;
+                    console.log('updated mapSrc', this.mapSrc);
+                }
+            }
+        },
+        methods: {
+            search() {
+                this.searchResults.display = true;
+            },
+            showMapView() {
+                console.log('show map view');
+                this.searchResults.activeView = 0;
+            },
+            showListView() {
+                console.log('show list view');
+                this.searchResults.activeView = 1;
+            }
+        },
+        mounted() {
+            console.log('mounted, setting post code to SW1H 9AJ');
+            this.searchResults.postCode = 'SW1H 9AJ';
         }
     }
 </script>
