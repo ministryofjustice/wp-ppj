89f6aa094e54de997404f0835dbc1506fd33fda9
James Devon <james.devon@digital.justice.gov.uk>
2017-11-06 12:00:32 +0000

    now copying images to public/img from src/img as part of the build process

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,7 +2,9 @@ const mix = require('laravel-mix');
 
 mix.js('src/js/main.js', 'public/js')
     .sourceMaps()
-    .sass('src/sass/main.sass', 'public/css');
+    .sass('src/sass/main.sass', 'public/css')
+    .copy('src/img/*', 'public/img/')
+;
 
 // mix.browserSync({
 //     proxy: 'localhost',
