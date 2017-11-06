f520fc924d1d6449bededa6feeb7d439e0fc692a
James Devon <james.devon@digital.justice.gov.uk>
2017-11-06 12:46:16 +0000

    including temporary basic auth for heroku username: papj, password: pass

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,9 +4,18 @@ mix.js('src/js/main.js', 'public/js')
     .sourceMaps()
     .sass('src/sass/main.sass', 'public/css')
     .copy('src/img/*', 'public/img/')
+    .copy('src/html/*', 'public/')
 ;
 
 // mix.browserSync({
 //     proxy: 'localhost',
 //     port: 8890
 // });
+
+if (process.env.MIX_HEROKU) {
+    console.log('MIX_HEROKU is true!');
+    mix.copy('temp-auth/.htaccess', 'public/.htaccess');
+    mix.copy('temp-auth/.htpasswd', 'public/.htpasswd');
+} else {
+    console.log('MIX_HEROKU is false!');
+}
