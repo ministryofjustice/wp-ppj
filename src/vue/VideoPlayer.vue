4beb41e5ea842eac11a61106b4edf933e35b8b83
James Devon <james.devon@digital.justice.gov.uk>
2017-11-20 17:18:32 +0000

    added video content to apply page; added link to practice maths page;

--- src/vue/VideoPlayer.vue
+++ src/vue/VideoPlayer.vue
@@ -1,11 +1,25 @@
 <template>
     <div class="video-player">
-        <div class="video-player__play-button"></div>
+        <!--<div class="video-player__play-button"></div>-->
+        <iframe v-if="src"
+                :src="src"
+                webkitallowfullscreen='true'
+                mozallowfullscreen='true'
+                allowfullscreen='true'
+        >
+            <p style="text-align:center">Iframes are required to view this content.</p>
+        </iframe>
     </div>
 </template>
 
 <script>
     export default {
+        props: {
+            src: {
+                type: String,
+                default: ''
+            }
+        },
         data() {
             return {
             }
