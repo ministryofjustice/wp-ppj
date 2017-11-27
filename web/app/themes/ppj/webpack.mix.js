e9a98af978f85695b85a8fd86c47d885ad7fee44
James Devon <james.devon@digital.justice.gov.uk>
2017-11-27 11:56:06 +0000

    modified files to fit new dir structure

--- web/app/themes/ppj/webpack.mix.js
+++ web/app/themes/ppj/webpack.mix.js
@@ -1,23 +1,13 @@
-const mix = require('laravel-mix');
-const dest = '../web/app/themes/ppj/';
-//const dest = 'public/';
+const
+    mix = require('laravel-mix'),
+    dest = 'dest/'
+;
 
-mix.js('src/js/main.js'       , dest + 'js')
-    .sass('src/sass/main.sass', dest + 'css')
-    .copy('src/img/*'         , dest + 'img/')
-    .copy('src/html/*'        , dest + '')
-    .copy('src/fonts/*'       , dest + 'fonts')
+mix.js('src/js/main.js'        , dest + 'js')
+    .sass('src/sass/main.sass' , dest + 'css')
+    .copy('src/img/*'          , dest + 'img/')
+    .copy('src/html/*'         , dest + '')
+    .copy('src/fonts/*'        , dest + 'fonts')
     .sourceMaps()
 ;
 
-// mix.browserSync({
-//     proxy: 'localhost',
-//     port: 8890
-// });
-
-if (process.env.MIX_HEROKU) {
-    console.log('MIX_HEROKU is true!');
-    mix.copy('temp-auth/.htaccess', 'public/.htaccess');
-} else {
-    console.log('MIX_HEROKU is false!');
-}
