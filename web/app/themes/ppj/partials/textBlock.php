5be44a5da7f1b66ca58b6879b400afa28b408d65
James Devon <james.devon@digital.justice.gov.uk>
2017-11-30 10:57:54 +0000

    text block elements only showing if they have content

--- web/app/themes/ppj/partials/textBlock.php
+++ web/app/themes/ppj/partials/textBlock.php
@@ -7,14 +7,23 @@ $td = $ppj_template_data;
 
 <div class="l-full">
     <div class="text-block">
+        <?php if ($td['title']): ?>
         <h2 class="text-block__title">
             <?= $td['title'] ?>
         </h2>
+        <?php endif; ?>
+
+        <?php if ($td['subtitle']): ?>
         <h3 class="text-block__subtitle">
             <?= $td['subtitle']?>
         </h3>
+        <?php endif; ?>
+
+        <?php if ($td['content']): ?>
         <div class="text-block__content">
             <?= $td['content']?>
         </div>
+        <?php endif; ?>
+        
     </div>
 </div>
