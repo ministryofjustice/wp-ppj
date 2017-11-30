8f5830bea79f4471195f7b8f8fa4848023c3ab13
James Devon <james.devon@digital.justice.gov.uk>
2017-11-30 17:50:38 +0000

    text blocks can now display full width quotes

--- web/app/themes/ppj/partials/textBlock.php
+++ web/app/themes/ppj/partials/textBlock.php
@@ -7,22 +7,47 @@ $td = $ppj_template_data;
 
 <div class="l-full">
     <div class="text-block">
-        <?php if ($td['title']): ?>
-        <h2 class="text-block__title">
-            <?= $td['title'] ?>
-        </h2>
+        <?php if ($td['type'] == 'regular'): ?>
+
+            <?php if ($td['title']): ?>
+                <h2 class="text-block__title">
+                    <?= $td['title'] ?>
+                </h2>
+            <?php endif; ?>
+
+            <?php if ($td['subtitle']): ?>
+                <h3 class="text-block__subtitle">
+                    <?= $td['subtitle']?>
+                </h3>
+            <?php endif; ?>
+
+            <?php if ($td['content']): ?>
+                <div class="text-block__content">
+                    <?= $td['content']?>
+                </div>
+            <?php endif; ?>
+
         <?php endif; ?>
 
-        <?php if ($td['subtitle']): ?>
-        <h3 class="text-block__subtitle">
-            <?= $td['subtitle']?>
-        </h3>
-        <?php endif; ?>
+        <?php if ($td['type'] == 'quote'): ?>
 
-        <?php if ($td['content']): ?>
-        <div class="text-block__content">
-            <?= $td['content']?>
-        </div>
+            <?php if ($td['quote']): ?>
+
+                <div class="text-block__quote">
+                    <div class="text-block__quote-mark"></div>
+
+                    <div class="text-block__quote-content">
+                        <?= $td['quote']?>
+                    </div>
+
+                    <?php if ($td['quote_source']): ?>
+                        <div class="text-block__quote-source">
+                            <?= $td['quote_source']?>
+                        </div>
+                    <?php endif; ?>
+                </div>
+            <?php endif; ?>
+            
         <?php endif; ?>
         
     </div>
