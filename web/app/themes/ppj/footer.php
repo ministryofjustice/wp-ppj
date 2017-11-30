00460f7047da84b19f479e92846c337af5cf672f
James Devon <james.devon@digital.justice.gov.uk>
2017-11-30 16:20:17 +0000

    added footer nav links

--- web/app/themes/ppj/footer.php
+++ web/app/themes/ppj/footer.php
@@ -1,5 +1,23 @@
 <?php ?>
 
+<div class="footer">
+    <div class="l-full">
+        <div class="footer__container">
+            <ul class="footer__nav-links">
+                <li class="footer__nav-link">
+                    <a href="">Contact</a>
+                </li>
+                <li class="footer__nav-link">
+                    <a href="">Terms & Conditions</a>
+                </li>
+                <li class="footer__nav-link">
+                    <a href="">Cookies</a>
+                </li>
+            </ul>
+            <div class="footer__copyright">&#xa9; 2017 HM Prison and Probation Service</div>
+        </div>
+    </div>
+</div>
 </page-container>
 </div>
 <?php wp_footer(); ?>
