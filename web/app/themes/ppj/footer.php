6a637b43107c8f0cd1082c1384d75f4d203417ad
James Devon <james.devon@digital.justice.gov.uk>
2017-12-12 16:16:39 +0000

    Removed contact link in footer; Added href urls for footer links;

--- web/app/themes/ppj/footer.php
+++ web/app/themes/ppj/footer.php
@@ -5,13 +5,10 @@
         <div class="footer__container">
             <ul class="footer__nav-links">
                 <li class="footer__nav-link">
-                    <a href="">Contact</a>
+                    <a href="/terms-conditions">Terms & Conditions</a>
                 </li>
                 <li class="footer__nav-link">
-                    <a href="">Terms & Conditions</a>
-                </li>
-                <li class="footer__nav-link">
-                    <a href="">Cookies</a>
+                    <a href="/cookies">Cookies</a>
                 </li>
             </ul>
             <div class="footer__copyright">&#xa9; 2017 HM Prison and Probation Service</div>
