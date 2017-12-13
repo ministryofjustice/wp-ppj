ae9864da7b3e98c0ea7f55cb7c36118d9d018975
James Devon <james.devon@digital.justice.gov.uk>
2017-12-13 16:59:37 +0000

    added font-weight 700 for .enormous elements

--- web/app/themes/ppj/header.php
+++ web/app/themes/ppj/header.php
@@ -27,7 +27,7 @@ $mainMenuJSON = json_encode(wp_get_nav_menu_items('Main menu'));
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <title><?php echo get_bloginfo( 'name' ); ?> - <?php the_title(); ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-    <link href="https://fonts.googleapis.com/css?family=Barlow:300,400,500,600" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Barlow:300,400,500,600,700" rel="stylesheet">
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDDplfBkLzNA3voskfGyExYnQ46MJ0VtpA"></script>
     <?php wp_head(); ?>
 </head>
