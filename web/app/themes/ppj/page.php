477ebc66943a9369609e78a0130516ae6b6a7028
James Devon <james.devon@digital.justice.gov.uk>
2017-12-22 11:25:57 +0000

    using flex to align page-blocks
    footer element is now fixed to the bottom of the page/screen

--- web/app/themes/ppj/page.php
+++ web/app/themes/ppj/page.php
@@ -4,7 +4,8 @@ get_header();
 $page_blocks = get_field('page_blocks');
 
 ?>
-
+  <div class="page-blocks">
     <?php echo ppj\renderPageBlockData($page_blocks); ?>
+  </div>
 
 <?php get_footer();
