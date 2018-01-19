2bddf2e99bb612000267049cd41aa9db19976f32
James Devon <james.devon@digital.justice.gov.uk>
2018-01-19 14:46:30 +0000

    wrapped text block in a container so a correctly sized separator can be added

--- web/app/themes/ppj/partials/textBlock.php
+++ web/app/themes/ppj/partials/textBlock.php
@@ -13,46 +13,48 @@ if (is_array($td['width'])) {
 <div class="<?= $layout ?>"
      id="<?= urlencode(strtolower($td['title'])) ?>"
 >
+    <div class="text-block-container">
+        <div class="text-block">
+            <?php if ($td['type'] == 'regular') : ?>
+                <?php if ($td['title']) : ?>
+                    <h2 class="text-block__title">
+                        <?= $td['title'] ?>
+                    </h2>
+                <?php endif; ?>
 
-    <div class="text-block">
-        <?php if ($td['type'] == 'regular') : ?>
-            <?php if ($td['title']) : ?>
-                <h2 class="text-block__title">
-                    <?= $td['title'] ?>
-                </h2>
-            <?php endif; ?>
+                <?php if ($td['subtitle']) : ?>
+                    <h3 class="text-block__subtitle">
+                        <?= $td['subtitle'] ?>
+                    </h3>
+                <?php endif; ?>
 
-            <?php if ($td['subtitle']) : ?>
-                <h3 class="text-block__subtitle">
-                    <?= $td['subtitle'] ?>
-                </h3>
-            <?php endif; ?>
-
-            <?php if ($td['content']) : ?>
-                <div class="text-block__content">
-                    <?= $td['content'] ?>
-                </div>
-            <?php endif; ?>
-
-        <?php endif; ?>
-
-        <?php if ($td['type'] == 'quote') : ?>
-            <?php if ($td['quote']) : ?>
-                <div class="text-block__quote">
-                    <div class="text-block__quote-mark"></div>
-
-                    <div class="text-block__quote-content">
-                        <?= $td['quote'] ?>
+                <?php if ($td['content']) : ?>
+                    <div class="text-block__content">
+                        <?= $td['content'] ?>
                     </div>
+                <?php endif; ?>
 
-                    <?php if ($td['quote_source']) : ?>
-                        <div class="text-block__quote-source">
-                            <?= $td['quote_source'] ?>
-                        </div>
-                    <?php endif; ?>
-                </div>
             <?php endif; ?>
-        <?php endif; ?>
+
+            <?php if ($td['type'] == 'quote') : ?>
+                <?php if ($td['quote']) : ?>
+                    <div class="text-block__quote">
+                        <div class="text-block__quote-mark"></div>
+
+                        <div class="text-block__quote-content">
+                            <?= $td['quote'] ?>
+                        </div>
+
+                        <?php if ($td['quote_source']) : ?>
+                            <div class="text-block__quote-source">
+                                <?= $td['quote_source'] ?>
+                            </div>
+                        <?php endif; ?>
+                    </div>
+                <?php endif; ?>
+            <?php endif; ?>
+
+        </div>
 
         <?php if (isset($td['link']) && isset($td['link']['url']) && isset($td['link']['title'])) : ?>
             <div class="text-block__link-container">
