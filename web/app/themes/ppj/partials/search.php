8b1f74e7fe0f59c883e60afbc7d2807b8ae55bec
James Devon <james.devon@digital.justice.gov.uk>
2018-01-21 12:35:09 +0000

    testing to make sure there is a title value for the search component before trying to display a title value

--- web/app/themes/ppj/partials/search.php
+++ web/app/themes/ppj/partials/search.php
@@ -3,10 +3,12 @@
 global $ppj_template_data;
 $td = $ppj_template_data;
 
+$title = (isset($td['title'])) ? $td['title'] : '';
+
 ?>
 
 <div class="l-full">
     <search default-search-term="<?= $td['default_search_term'] ?>"
-            title="<?= $td['title'] ?>"
+            title="<?= $title ?>"
     ></search>
 </div>
