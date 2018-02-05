d8ec005c8a3e2f06915bd6230b7ef047cbdf97f4
James Devon <james.devon@digital.justice.gov.uk>
2018-02-05 09:57:24 +0000

    Added ACF field to allow HTML meta description to be added to <head> for each page

--- web/app/themes/ppj/header.php
+++ web/app/themes/ppj/header.php
@@ -2,6 +2,7 @@
 global $post;
 
 $htmlTitle = get_field('html_title');
+$htmlMetaDescription = get_field('html_meta_description');
 
 // header text
 $header_text    = get_field( 'header_text' );
@@ -46,6 +47,7 @@ $mainMenuJSON = json_encode( $filteredNavMenuItems );
     <meta charset="<?php bloginfo('charset'); ?>">
     <title><?= $htmlTitle ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="description" content="<?= $htmlMetaDescription ?>" />
     <link href="https://fonts.googleapis.com/css?family=Barlow:300,400,500,600,700" rel="stylesheet">
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDDplfBkLzNA3voskfGyExYnQ46MJ0VtpA"></script>
     <?php wp_head(); ?>
