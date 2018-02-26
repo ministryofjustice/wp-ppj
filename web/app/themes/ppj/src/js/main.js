a0cd5ab387f83048872190b009571cdd2d0e1364
James Devon <james.devon@digital.justice.gov.uk>
2018-02-26 18:47:35 +0000

    removing accordion lorem ipsum generator code

--- web/app/themes/ppj/src/js/main.js
+++ web/app/themes/ppj/src/js/main.js
@@ -42,14 +42,6 @@ window.addEventListener('load', function() {
 
   if (document.querySelectorAll('#site-container').length > 0) {
 
-    (function addAccordionFillerContent() {  // TODO remove this before launch
-      const elements = document.getElementsByClassName('accordion__list-element js');
-      for (let i = 0; i < elements.length; i++) {
-        const content = elements[i].getElementsByClassName('accordion__list-element-content')[0];
-        content.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
-      }
-    })();
-
     Vue.component('accordion', Accordion);
     Vue.component('accordion-element', Accordion.childComponents.Element);
     Vue.component('contact', Contact);
