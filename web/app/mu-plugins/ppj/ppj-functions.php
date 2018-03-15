e98bb960b4fab84dd8912d7391806337bd008fb1
James Devon <james.devon@digital.justice.gov.uk>
2018-03-15 17:01:59 +0000

    removed redundant createPostTypes function

--- web/app/mu-plugins/ppj/ppj-functions.php
+++ web/app/mu-plugins/ppj/ppj-functions.php
@@ -1,25 +1,6 @@
 <?php
 namespace ppj;
 
-// TODO remove
-function createPostTypes()
-{
-    $name = 'position';
-    $capitalizedName = ucfirst($name);
-    register_post_type(
-        'ppj_' . $name,
-        array(
-            'labels' => array(
-                'name' => __($capitalizedName . 's'),
-                'singular_name' => __($capitalizedName)
-            ),
-            'public' => true,
-            'has_archive' => true,
-        )
-    );
-}
-add_action('init', __NAMESPACE__ . '\\createPostTypes');
-
 function template($data, $slug, $name = '')
 {
     global $ppj_template_data;
