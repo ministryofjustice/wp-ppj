5139b4e4611c03bff69b21e6a1bba363c67b7e57
James Devon <james.devon@digital.justice.gov.uk>
2018-03-20 17:32:28 +0000

    fixed botched rebase

--- web/app/themes/ppj/webpack.mix.js
+++ web/app/themes/ppj/webpack.mix.js
@@ -1,6 +1,6 @@
 const mix = require('laravel-mix');
 
-mix.js('src/js/main.js'        , dest + 'js')
+mix.js('src/js/main.js'        , 'dest/js')
   .sass('src/sass/landing-page.sass'   , 'dest/css/landing-page.css')
   .sass('src/sass/prison-officer.sass' , 'dest/css/prison-officer.css')
   .sass('src/sass/youth-custody.sass'  , 'dest/css/youth-custody.css')
