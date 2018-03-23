de5e8360bee540bbc0e9053e9415c4846c6f36ac
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2018-03-23 13:13:33 +0000

    Add static HTML error pages

--- web/app/themes/ppj/webpack.mix.js
+++ web/app/themes/ppj/webpack.mix.js
@@ -4,6 +4,7 @@ mix.setPublicPath('./dest/');
 
 mix.js('src/js/main.js'        , 'dest/js')
     .sass('src/sass/main.sass' , 'dest/css')
+    .sass('src/sass/error-page.sass', 'dest/css')
     .copy('src/img/*'          , 'dest/img/')
     .copy('src/img/svg/*'      , 'dest/img/svg/')
 ;
