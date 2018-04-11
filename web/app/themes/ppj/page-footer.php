d48c0723745917376cfb7e5e089606e01fb5746d
James Devon <james.devon@digital.justice.gov.uk>
2018-04-11 14:49:32 +0100

    changed </page-container> to </div> in page-footer.php

--- web/app/themes/ppj/page-footer.php
+++ web/app/themes/ppj/page-footer.php
@@ -6,32 +6,31 @@ $footerCopyright = get_field('footer_copyright', 'option');
 
 ?>
 
-    <div class="l-full l-full--footer">
-        <?php if ($facebookContent): ?>
-            <div class="facebook-footer">
-                <div class="facebook-footer__container">
-                    <div class="facebook-footer__icon">
-                        <?php ppj\inlineSVG('facebook'); ?>
-                    </div>
-                    <div class="facebook-footer__content">
-                        <?= $facebookContent ?>
-                    </div>
-                </div>
-            </div>
-        <?php endif; ?>
-
-        <?php if ($footerContent || $footerCopyright): ?>
-            <div class="footer">
-                <div class="footer__container">
-                    <?= $footerContent; ?>
-                    <div class="footer__copyright">
-                        <span><?= $footerCopyright; ?></span>
+        <div class="l-full l-full--footer">
+            <?php if ($facebookContent): ?>
+                <div class="facebook-footer">
+                    <div class="facebook-footer__container">
+                        <div class="facebook-footer__icon">
+                            <?php ppj\inlineSVG('facebook'); ?>
+                        </div>
+                        <div class="facebook-footer__content">
+                            <?= $facebookContent ?>
+                        </div>
                     </div>
                 </div>
-            </div>
-        <?php endif; ?>
-    </div>
+            <?php endif; ?>
 
-    </page-container>
+            <?php if ($footerContent || $footerCopyright): ?>
+                <div class="footer">
+                    <div class="footer__container">
+                        <?= $footerContent; ?>
+                        <div class="footer__copyright">
+                            <span><?= $footerCopyright; ?></span>
+                        </div>
+                    </div>
+                </div>
+            <?php endif; ?>
+        </div>
+    </div> <?php // page-container ?>
 </div><?php // site-container ?>
 <?php get_footer();
