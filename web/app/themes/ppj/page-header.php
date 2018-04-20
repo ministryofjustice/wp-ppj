140dc5eadad0855a3c20aab971f317e1bd3bc745
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2018-04-20 13:34:34 +0100

    Stop header hero from showing up to the party when it isn't invited
    The div `.header__hero` was still present on pages which don't use this element. However due to a recent CSS change, this element now has a minimum height – so still has a height even when it contains no content. This unwanted presence was causing it to block clicks on the content below. This became problematic on the 'Find A Job' page where the map zoom buttons sat directly underneath this ghost element.

--- web/app/themes/ppj/page-header.php
+++ web/app/themes/ppj/page-header.php
@@ -28,37 +28,39 @@ $headerClass .= ($headerStyle == 'dark') ?  'header--style-dark' : '';
         <div class="header <?= $headerClass ?>">
             <?= ppj\partial(null, 'headerNavigationMenu') ?>
 
-            <div class="header__hero">
-                <div class="header__img-ratio">
-                    <?php if ($headerImageData): ?>
-                        <?= ppj\partial(
-                                [
-                                    'imageData' => $headerImageData,
-                                    'mobileImageData' => $headerImageMobileData
-                                ],
-                            'headerHeroImage')
-                        ?>
-                    <?php endif; ?>
+            <?php if ($headerImageData || $header_text): ?>
+                <div class="header__hero">
+                    <div class="header__img-ratio">
+                        <?php if ($headerImageData): ?>
+                            <?= ppj\partial(
+                                    [
+                                        'imageData' => $headerImageData,
+                                        'mobileImageData' => $headerImageMobileData
+                                    ],
+                                'headerHeroImage')
+                            ?>
+                        <?php endif; ?>
 
-                    <?php if ($header_text): ?>
-                        <div class="header__overlay">
-                            <div class="l-full">
-                                <div class="header__text-container">
-                                    <?php if ($header_text): ?>
-                                        <div class="header__text">
-                                            <?= $header_text ?>
-                                        </div>
-                                    <?php endif; ?>
-                                    <?php if ($header_subtext): ?>
-                                        <div class="header__subtext">
-                                            <?= $header_subtext ?>
-                                        </div>
-                                    <?php endif; ?>
+                        <?php if ($header_text): ?>
+                            <div class="header__overlay">
+                                <div class="l-full">
+                                    <div class="header__text-container">
+                                        <?php if ($header_text): ?>
+                                            <div class="header__text">
+                                                <?= $header_text ?>
+                                            </div>
+                                        <?php endif; ?>
+                                        <?php if ($header_subtext): ?>
+                                            <div class="header__subtext">
+                                                <?= $header_subtext ?>
+                                            </div>
+                                        <?php endif; ?>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    <?php endif; ?>
+                        <?php endif; ?>
+                    </div>
                 </div>
-            </div>
+            <?php endif; ?>
 
         </div>
