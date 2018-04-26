42a23b74cedc8ea31920886649da49e108e08218
James Devon <james.devon@digital.justice.gov.uk>
2018-04-26 16:16:58 +0100

    new navigation menu
    created a site wide navigation menu in addition to the leg specific nav menu;
    The leg menu is now position relatively so it overlays the header image
    
    A new mobile menu was created which is an amalgam of the two menus;
    the mobile nav exists in its own template mobileNavitation.php;
    
    .header__nav-menu -> .header__nav-leg-menu;
    
    the flag for whether the mobile nav menu is open is now a body class called .nav-menu-is-open;
    
    $color-main for the landing page is now white;

--- web/app/themes/ppj/partials/headerNavigationMenu.php
+++ web/app/themes/ppj/partials/headerNavigationMenu.php
@@ -1,40 +1,52 @@
 <?php
-$navMenuItems = wp_get_nav_menu_items( ppj\getLegNameFromPath() );
+global $post;
 
-$logoTargetUrl = ppj\getLogoTargetURL();
-
-$filteredNavMenuItems = [];
-if (isset($navMenuItems) && !!$navMenuItems ) {
-    foreach ( $navMenuItems as $item ) {
-        $same = ( isset( $post ) && $post->ID == $item->object_id );
-
-        $filteredNavMenuItems[] = [
-            'title'    => $item->title,
-            'url'      => $item->url,
-            'selected' => $same
-        ];
-    }
-}
+$siteWideNavMenuItems = wp_get_nav_menu_items('site-wide-nav');
+$modifiedSiteWideNavItems = ppj\markCurrentlySelectedAncestorMenuItem($siteWideNavMenuItems);
 ?>
 
 <div class="l-site-header">
     <div class="header__site-header">
-        <div class="header__site-header-content">
-            <a href="<?= $logoTargetUrl ?>" class="header__logo"></a>
-            <div class="header__nav-link js" onclick="ppj.openNavMenu()">menu</div>
-
-            <div class="header__nav-menu">
-                <button class="header__nav-menu-close-button js" onclick="ppj.closeNavMenu()"></button>
-                <ul class="header__nav-menu-list">
-                    <?php foreach ($filteredNavMenuItems as $item): ?>
-                        <li class="header__nav-menu-list-element <?= ($item['selected']) ? 'header__nav-menu-list-element--selected' : ''?>">
-                            <a href="<?= $item['url'] ?>">
-                                <span class="header__nav-menu-list-element-text"><?= $item['title'] ?></span>
-                            </a>
-                        </li>
-                    <?php endforeach; ?>
-                </ul>
-            </div>
+        <div class="header__site-wide-nav">
+            <ul class="header__site-wide-nav-menu-list">
+                <?php foreach ($modifiedSiteWideNavItems as $item):
+                    $listElementClass = 'header__site-wide-nav-menu-list-element';
+                    $listElementClass .= ($item['selected']) ? ' header__site-wide-nav-menu-list-element--selected' : '';
+                    ?>
+                    <li class="<?= $listElementClass ?>">
+                        <a href="<?= $item['url'] ?>"
+                           class="header__site-wide-nav-menu-list-element-link">
+                            <span class="header__site-wide-nav-menu-list-element-text"><?= $item['title'] ?></span>
+                        </a>
+                        <span class="header__nav-link js" onclick="ppj.openNavMenu()">menu</span>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
         </div>
+
+        <?php if (ppj\onLeg()):
+            $logoTargetUrl = ppj\getLegHomeRelativePath();
+            $legNavMenuItems = wp_get_nav_menu_items( ppj\getLegNameFromPath() );
+            $modifiedLegNavMenuItems = ppj\markCurrentlySelectedMenuItem($legNavMenuItems);
+            ?>
+
+            <div class="header__leg-nav">
+                <a href="<?= $logoTargetUrl ?>" class="header__logo"></a>
+
+                <div class="header__nav-leg-menu">
+                    <button class="header__nav-menu-close-button js" onclick="ppj.closeNavMenu()"></button>
+                    <ul class="header__nav-leg-menu-list">
+
+                        <?php foreach ($modifiedLegNavMenuItems as $item): ?>
+                            <li class="header__nav-leg-menu-list-element <?= ($item['selected']) ? 'header__nav-leg-menu-list-element--selected' : ''?>">
+                                <a href="<?= $item['url'] ?>">
+                                    <span class="header__nav-leg-menu-list-element-text"><?= $item['title'] ?></span>
+                                </a>
+                            </li>
+                        <?php endforeach; ?>
+                    </ul>
+                </div>
+            </div>
+        <?php endif; ?>
     </div>
 </div>
