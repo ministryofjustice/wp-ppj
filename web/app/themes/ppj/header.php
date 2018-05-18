8764a9f714ac26595da9fc2040eeb4bc29fef7b7
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2018-05-18 16:36:37 +0100

    Add Google Tag Manager to main page layout.

--- web/app/themes/ppj/header.php
+++ web/app/themes/ppj/header.php
@@ -20,6 +20,15 @@ if (LegNav\onLeg())                          $bodyClasses[] = 'leg';
     <title><?= $htmlTitle ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="description" content="<?= $htmlMetaDescription ?>" />
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-N2SCJMC');</script>
+    <!-- End Google Tag Manager -->
+
     <?php wp_head(); ?>
     <script><?php // code that must be available before the HTML has finished loading. ?>
         window.ppj = {};
@@ -40,3 +49,7 @@ if (LegNav\onLeg())                          $bodyClasses[] = 'leg';
 </head>
 
 <body <?php body_class($bodyClasses); ?>>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N2SCJMC"
+                      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
