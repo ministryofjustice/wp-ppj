cddaf671a4ce76075cd741c76aaa5ac95c726967
James Devon <james.devon@digital.justice.gov.uk>
2018-05-29 14:02:00 +0100

    fixed mobile nav for landing page not showing
    refactored the mobile nav menu, so that it is now composed of two sections:
    the primary section at the top, and the secondary section at the bottom.
    
    On legs, the primary section will display the leg nav menu,
    whereas on the landing page, the primary section will display the site wide nav menu.
    
    On legs, the secondary section will display the site wide nav menu.

--- web/app/themes/ppj/partials/mobileNavigation.php
+++ web/app/themes/ppj/partials/mobileNavigation.php
@@ -1,70 +1,78 @@
 <?php
 use ppj\LegNav;
+
+// the mobile nav always needs to show the site wide nav
+$modifiedSiteWideNavItems = ppj\markCurrentlySelectedAncestorMenuItem(ppj\navMenuItems('site-wide'));
+
+// get a handle on the link that represents the current leg
+$currentLegItem = false;
+
+// if the user is currently on a leg
+//   then the leg nav is in the primary position
+//   and the site wide nav is in the secondary position
+if (LegNav\onLeg()) {
+    $primaryNavMenuItems = ppj\markCurrentlySelectedMenuItem(ppj\navMenuItems( LegNav\legName() ));
+    $secondaryNavMenuItems = $modifiedSiteWideNavItems;
+
+    // identify the currently selected leg link
+    // so it can be inserted into the primary menu
+    foreach ($modifiedSiteWideNavItems as $item) {
+        if ($item['selected']) {
+            $currentLegItem = $item;
+        }
+    }
+
+// otherwise just show the site wide nav in the primary position
+} else {
+    $primaryNavMenuItems = $modifiedSiteWideNavItems;
+}
+
 ?>
 
 <div class="mobile-nav">
     <div class="mobile-nav__overlay"></div>
-    <?php
-    $legNavMenuItems = ppj\navMenuItems( LegNav\legName() );
-    $modifiedLegNavMenuItems = ppj\markCurrentlySelectedMenuItem($legNavMenuItems);
-    $currentLegItem = false;
-    $isOnLeg = LegNav\onLeg();
-
-    if ($isOnLeg) {
-        $siteWideNavMenuItems = ppj\navMenuItems('site-wide');
-        $modifiedSiteWideNavItems = ppj\markCurrentlySelectedAncestorMenuItem($siteWideNavMenuItems);
-
-        foreach ($modifiedSiteWideNavItems as $item) {
-            if ($item['selected']) {
-                $currentLegItem = $item;
-            }
-        }
-    }
-    ?>
     <button class="mobile-nav__close-button"
             onclick="ppj.closeNavMenu()">
     </button>
-    <div class="mobile-nav__leg-nav">
-        <div class="mobile-nav__menu">
-            <ul class="mobile-nav__menu-list">
-                <?php
-                if ($currentLegItem): ?>
-                    <li class="<?= 'mobile-nav__current-leg-element' ?>">
-                        <a href="<?= $currentLegItem['url'] ?>"
-                           class="mobile-nav__menu-list-element-link">
-                            <span class="mobile-nav__current-leg-element-text"><?= $currentLegItem['title'] ?></span>
-                        </a>
-                    </li>
-                <?php endif;
+    <nav class="mobile-nav__menu">
+        <ul class="mobile-nav__menu-list mobile-nav__menu-list--primary">
+            <?php
+            if ($currentLegItem): ?>
+                <li class="<?= 'mobile-nav__current-leg-element' ?>">
+                    <a href="<?= $currentLegItem['url'] ?>"
+                       class="mobile-nav__menu-list-element-link">
+                        <span class="mobile-nav__current-leg-element-text"><?= $currentLegItem['title'] ?></span>
+                    </a>
+                </li>
+            <?php endif;
 
-                foreach ($modifiedLegNavMenuItems as $item):
+            foreach ($primaryNavMenuItems as $item):
+                $elementClass = "mobile-nav__menu-list-element";
+                $elementClass.= ($item['selected']) ? ' mobile-nav__menu-list-element--selected' : '';
+                ?>
+                <li class="<?= $elementClass ?>">
+                    <a href="<?= $item['url'] ?>"
+                       class="mobile-nav__menu-list-element-link"
+                    >
+                        <span class="mobile-nav__menu-list-element-text"><?= $item['title'] ?></span>
+                    </a>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+        <?php if (isset($secondaryNavMenuItems)): ?>
+            <ul class="mobile-nav__menu-list mobile-nav__menu-list--secondary">
+                <?php foreach ($secondaryNavMenuItems as $item):
                     $elementClass = "mobile-nav__menu-list-element";
-                    $elementClass.= ($item['selected']) ? ' mobile-nav__menu-list-element--selected' : '';
-                    ?>
-                    <li class="<?= $elementClass ?>">
-                        <a href="<?= $item['url'] ?>"
-                           class="mobile-nav__menu-list-element-link"
-                        >
-                            <span class="mobile-nav__menu-list-element-text"><?= $item['title'] ?></span>
-                        </a>
-                    </li>
+                    if (!$item['selected']): ?>
+                        <li class="<?= $elementClass ?>">
+                            <a href="<?= $item['url'] ?>"
+                               class="mobile-nav__menu-list-element-link">
+                                <span class="mobile-nav__menu-list-element-text"><?= $item['title'] ?></span>
+                            </a>
+                        </li>
+                    <?php endif; ?>
                 <?php endforeach; ?>
             </ul>
-            <?php if ($isOnLeg): ?>
-                <ul class="mobile-nav__menu-list mobile-nav__menu-list--site-wide">
-                    <?php foreach ($modifiedSiteWideNavItems as $item):
-                        $elementClass = "mobile-nav__menu-list-element";
-                        if (!$item['selected']): ?>
-                            <li class="<?= $elementClass ?>">
-                                <a href="<?= $item['url'] ?>"
-                                   class="mobile-nav__menu-list-element-link">
-                                    <span class="mobile-nav__menu-list-element-text"><?= $item['title'] ?></span>
-                                </a>
-                            </li>
-                        <?php endif; ?>
-                    <?php endforeach; ?>
-                </ul>
-            <?php endif; ?>
-        </div>
-    </div>
+        <?php endif; ?>
+    </nav>
 </div>
