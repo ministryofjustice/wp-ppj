09f146c0b5a6c1e577170200bdbd392c9b33f531
James Devon <james.devon@digital.justice.gov.uk>
2018-06-01 14:45:13 +0100

    preserving search term and marker for inter find-a-job page link; removed job feed message ACF fields;
    Now when the user clicks on the inter find-a-job page link
    (from /youth-custody/find-a-job/ to /prison-officer/find-a-job)
    the search term and search term marker will carried to the new page.
    
    Also, the ACF fields for specifying the the job feed URL,
    and the inter find-a-job page fields have been removed.
    These values have been hardcoded into the Vue component.

--- web/app/themes/ppj/find-a-job.php
+++ web/app/themes/ppj/find-a-job.php
@@ -4,15 +4,13 @@
  */
 
 include 'page-header.php';
-//ppj\dump('job_list_message ' . get_field('job_list_message'));
+
 ?>
 
 <div class="find-a-job-container">
     <find-a-job
-            job-feed-url="<?= get_field('job_feed_url')?>"
-            job-list-message="<?= get_field('job_list_message') ?>"
-            job-list-message-url="<?= get_field('job_list_message_url') ?>"
-            job-title="<?= get_field('job_title') ?>"
+        job-title="<?= get_field('job_title') ?>"
+        leg="<?= ppj\LegNav\legName()?>"
     ></find-a-job>
 </div>
 
