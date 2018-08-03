e7d53bdd11b3c2b8d5efcd5f6ecf70ac2d6b3e8f
James Devon <james.devon@digital.justice.gov.uk>
2018-08-03 17:09:21 +0100

    Converting SurveyMonkey to Google Forms

---
+++ web/app/themes/ppj/partials/userSurveyScript.php
@@ -0,0 +1,60 @@
+<?php
+
+// Only show the survey if it is active
+$activateSurvey = get_field('activate_survey', 'option');
+if (!$activateSurvey) {
+    return;
+}
+
+// Only show the survey on the specified pages
+$activePages = get_field('active_pages', 'option');
+$isActivePage = empty($activePages) || in_array(get_the_ID(), $activePages);
+if (!$isActivePage) {
+    return;
+}
+
+$displayPercentage = get_field('display_percentage', 'option');
+$popupDelayMilliseconds = 1000 * get_field('popup_delay', 'option');
+$surveyURL = get_field('survey_url', 'option');
+
+?>
+<div class="screen-overlay screen-overlay--user-feedback-survey">
+    <div class="screen-overlay__content">
+        <div class="screen-overlay__close-button">Close</div>
+        <div class="screen-overlay__iframe-container"></div>
+    </div>
+</div>
+<script>
+    const activateSurvey = () => {
+        const body = document.querySelector('body');
+        const overlay = document.querySelector('.screen-overlay--user-feedback-survey');
+        const iframe = document.createElement('iframe');
+
+        body.classList.add('site-overlay-is-active');
+        iframe.src = '<?= $surveyURL ?>';
+        overlay.querySelector('.screen-overlay__iframe-container').appendChild(iframe);
+        overlay.classList.add('screen-overlay--active');
+
+        // Add callback to close overlay when user clicks the overlay
+        // This also includes the close button by default
+        overlay.addEventListener('click', ()=>{
+            overlay.classList.remove('screen-overlay--active');
+            body.classList.remove('site-overlay-is-active');
+        });
+    };
+
+    /*
+     * The survey should only be shown for a specified percentage of users.
+     * This calculation is being done in Javascript so that
+     * HTML caching does not enforce the same calculation result
+     * for all users until the cache expires
+     */
+    const randNumber = Math.floor(Math.random() * 100) + 1;
+    if (<?= $displayPercentage ?> >= randNumber) {
+        // Load the Google Forms survey in an iframe on page load ...
+        window.addEventListener('load', ()=>{
+            // ... after the specified delay
+            setTimeout(activateSurvey, <?= $popupDelayMilliseconds ?>)
+        });
+    }
+</script>
