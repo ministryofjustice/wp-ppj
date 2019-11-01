911ee07e416b09616ccb010a8e255ead6f9710ea
James Devon <james.devon@digital.justice.gov.uk>
2019-11-01 13:59:00 +0000

    Fix whitespace formatting

--- web/app/themes/ppj/page-footer.php
+++ web/app/themes/ppj/page-footer.php
@@ -25,62 +25,56 @@ switch (LegNav\legName()) {
 $isPage = (basename(get_page_template()) === 'page.php');
 
 ?>
-    <?php if ( basename(get_page_template()) !== 'find-a-job.php'): ?>
-        <div class="l-full">
-            <ul class="awards">
+        <?php if ( basename(get_page_template()) !== 'find-a-job.php'): ?>
+            <div class="l-full">
+                <ul class="awards">
 
-                <?php foreach($awards as $award): ?>
+                    <?php foreach($awards as $award): ?>
 
-                    <?php if(isset($award['image'])): ?>
+                        <?php if(isset($award['image'])): ?>
 
-                        <li class="award">
-                            <?= wp_get_attachment_image( $award['image']['id'], 'award'  ); ?>
-                        </li>
+                            <li class="award">
+                                <?= wp_get_attachment_image( $award['image']['id'], 'award'  ); ?>
+                            </li>
 
-                    <?php endif; ?>
+                        <?php endif; ?>
 
-                <?php endforeach; ?>
+                    <?php endforeach; ?>
 
-            </ul>
+                </ul>
+            </div>
+        <?php endif; ?>
+        <div class="l-full l-full--footer">
+            <?php if ( $isPage && $facebookFooterContent): ?>
+                <div class="facebook-footer">
+                    <div class="facebook-footer__container">
+                        <div class="facebook-footer__icon">
+                            <?php ppj\inlineSVG('facebook'); ?>
+                        </div>
+                        <div class="facebook-footer__content">
+                            <?= $facebookFooterContent ?>
+                        </div>
+                    </div>
+                </div>
+            <?php endif; ?>
+
+            <?php if ($footerContent || $footerCopyright): ?>
+                <div class="footer">
+                    <div class="footer__container">
+                        <?= $footerContent; ?>
+                        <div class="footer__logo">
+                            <img src="<?= get_template_directory_uri() ?>/dist/img/svg/logo.svg" alt="HM Prison and Probation Service logo"/>
+                        </div>
+                        <div class="footer__copyright">
+                            <span><?= $footerCopyright; ?></span>
+                        </div>
+                    </div>
+                </div>
+
+            <?php endif; ?>
+
         </div>
-    <?php endif; ?>
-    <div class="l-full l-full--footer">
-        <?php if ( $isPage && $facebookFooterContent): ?>
-            <div class="facebook-footer">
-                <div class="facebook-footer__container">
-                    <div class="facebook-footer__icon">
-                        <?php ppj\inlineSVG('facebook'); ?>
-                    </div>
-                    <div class="facebook-footer__content">
-                        <?= $facebookFooterContent ?>
-                    </div>
-                </div>
-            </div>
-        <?php endif; ?>
-
-        <?php if ($footerContent || $footerCopyright): ?>
-            <div class="footer">
-                <div class="footer__container">
-                    <?= $footerContent; ?>
-                    <div class="footer__logo">
-                        <img src="<?= get_template_directory_uri() ?>/dist/img/svg/logo.svg" alt="HM Prison and Probation Service logo"/>
-                    </div>
-                    <div class="footer__copyright">
-                        <span><?= $footerCopyright; ?></span>
-                    </div>
-                </div>
-            </div>
-
-        <?php endif; ?>
-
-        <?php if( isset($awards) ): ?>
-        <pre><?php //print_r($awards); ?></pre>
-
-        <?php endif; ?>
-    </div>
-</div>
-
-<?php // page-container ?>
+    </div> <?php // page-container ?>
     <?= ppj\partial(null,'mobileNavigation') ?>
 </div><?php // site-container ?>
 
